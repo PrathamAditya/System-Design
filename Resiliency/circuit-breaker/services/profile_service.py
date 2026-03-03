05e753ee4aff4d386fd75b538302793160086456
PrathamAditya <prathamadityasalhotra@gmail.com>
2026-03-03 23:57:15 +0530

    First iteration of circut breakers! with shitty check of health service.

--- Resiliency/circuit-breaker/services/profile_service.py
+++ Resiliency/circuit-breaker/services/profile_service.py
@@ -1,15 +1,19 @@
 from services.post_service import PostService
 
 class ProfileService:
+    
+
     def __init__(self, db):
         self.db = db
         self.post_service = PostService(db)
 
     def get_profile_with_posts(self, profile_id: str):
-        profile = self.db.profiles.find_one(
-            {"profile_id": profile_id},
-            {"_id": 0}
-        )
+        profile_service_healthy = True ## Very shitty check!
+        if profile_service_healthy == True:
+            profile = self.db.profiles.find_one(
+                {"profile_id": profile_id},
+                {"_id": 0}
+            )
 
         if not profile:
             return None
